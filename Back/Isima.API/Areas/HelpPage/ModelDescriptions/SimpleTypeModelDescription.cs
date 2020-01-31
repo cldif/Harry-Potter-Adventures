635479f84c42cd8499d2dc5fd03f57afa8427b38
Sylvain BESSONNEAU <sybessonne@local.isima.fr>
2020-01-31 16:15:21 +0100

    back deleted

--- Back/Isima.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Isima.API.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- Back/Isima.API/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Isima.API.WebApiApplication" Language="C#" %>
