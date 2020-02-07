1f6b789a86e4de05b03279e32a8d770fda114a8f
Yannis <yannis.mahiou1@mail.com>
2020-02-07 16:22:32 +0100

    Routing for front

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,11 +1,47 @@
+// IMPORTS FOR THE ROUTES
+
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { GameComponent } from './game/game.component';
+import { SettingsComponent } from './settings/settings.component';
+import { AdminComponent } from './admin/admin.component';
+import { HealthComponent } from './health/health.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+// ROUTE LIST
+export const routes: Routes = [
+  { 
+    path: 'game', 
+    component: GameComponent 
+  },
+  { 
+    path: 'settings', 
+    component: SettingsComponent 
+  },
+  { 
+    path: 'admin', 
+    component: AdminComponent 
+  },
+  { 
+    path: 'health', 
+    component: HealthComponent 
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
-const routes: Routes = [];
-
+// MODULES
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes) 
+  ],
+  exports: [
+    RouterModule
+  ]
 })
-export class AppRoutingModule { }
+
+// EXPORT THE CLASS
+export class AppRoutingModule { 
+}
--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule, routes } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+
+import { GameComponent } from './game/game.component';
+import { SettingsComponent } from './settings/settings.component';
+import { AdminComponent } from './admin/admin.component';
+import { HealthComponent } from './health/health.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    SettingsComponent,
+    AdminComponent,
+    HealthComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
