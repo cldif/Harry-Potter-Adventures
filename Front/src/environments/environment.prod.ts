584d310b2b0c74fc75a4cc11ce1b1e94616c4b13
Clément Dif <clement.dif@etu.isima.fr>
2020-02-21 16:54:40 +0100

    :zap: Improved the game interface by using promises for requests to the REST API.

--- Front/src/environments/environment.prod.ts
+++ Front/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "https://harrypotterapiisima.azurewebsites.net"
+  apiUrl: "https://harrypotterapiisima.azurewebsites.net",
+  firstScenarioId: 1
 };
