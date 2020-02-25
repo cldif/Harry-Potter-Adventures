d16f95ecf40767e0d0a4e8c568ca0c049a6dd73d
Clément Dif <clement.dif@etu.isima.fr>
2020-02-25 17:22:33 +0100

    :children_crossing: The user can now specify the server url and port as well as the identifier of the first scenario in the settings component.

--- Front/src/app/settings/settings.component.ts
+++ Front/src/app/settings/settings.component.ts
@@ -1,27 +1,33 @@
 import { Component, OnInit } from "@angular/core";
-import { environment } from '../../environments/environment'; 
-import { HttpClient } from "@angular/common/http";
-import { Router } from "@angular/router";
+import { FormBuilder } from "@angular/forms";
+
+import { SettingsService } from "../services/settings.service";
 
 @Component({
   selector: "app-settings-component",
-
   templateUrl: "settings.component.html",
-
   styleUrls: ["settings.component.css"]
 })
-
 export class SettingsComponent implements OnInit {
-  constructor(private router: Router) {}
+  form;
 
-  url: string;
-
-  port: string;
+  constructor(
+    private formBuilder: FormBuilder,
+    private settingsService: SettingsService
+  ) {
+    this.form = this.formBuilder.group({
+      url: this.settingsService.getServerURL(),
+      port: this.settingsService.getServerPort(),
+      firstScenarioId: this.settingsService.getFirstScenario()
+    });
+  }
 
   ngOnInit() {}
 
-  changeBackUrl(): void {
-    var new_url = this.url;
-    environment.apiUrl =  this.url; 
+  onSubmit(formData) {
+    this.settingsService.setServerURL(formData.url);
+    this.settingsService.setServerPort(formData.port);
+    this.settingsService.setFirstScenario(formData.firstScenarioId);
+    console.log("Paramètres enregistrés avec succès !", formData);
   }
 }
